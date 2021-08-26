3f27f6faca4f562cbea6b55bf01a1dca08f13064
Ali Bazrafshan <drerl@tuta.io>
2021-08-26 15:06:20 +0430

    Preserve color scheme between sessions

--- assets/color-scheme.js
+++ assets/color-scheme.js
@@ -1,16 +1,43 @@
-/*
+/*!
  * color-scheme.js
  */
 
-let darkMode = true;
-
-function toggleColor() {
-    if (darkMode) {
-        document.head.getElementsByTagName("link").item(1).href = "assets/css/light.min.css";
-    } else {
-        document.head.getElementsByTagName("link").item(1).href = "assets/css/dark.min.css";
+/*
+ * set color scheme
+ */
+function setColorScheme(cs) {
+    if (cs === 'dark')  {
+        document.head.getElementsByTagName("link").item(1).href = "assets/dark.min.css";
+    } else if (cs === 'light') {
+        document.head.getElementsByTagName("link").item(1).href = "assets/light.min.css";
     }
-    darkMode = !darkMode;
 }
 
-document.getElementById("color-scheme-toggle").addEventListener("click", toggleColor);
+/*
+ * refresh color scheme
+ */
+function refreshColorScheme() {
+    let cs = localStorage.getItem('colorScheme');
+    if (cs === null) {
+        cs = 'dark';
+    }
+    setColorScheme(cs);
+    localStorage.setItem('colorScheme', cs);
+}
+
+/*
+ * toggle color scheme
+ */
+function toggleColorScheme() {
+    let cs = localStorage.getItem('colorScheme');
+    if (cs === 'dark') {
+        cs = 'light';
+    } else if (cs === 'light') {
+        cs = 'dark';
+    }
+    setColorScheme(cs);
+    localStorage.setItem('colorScheme', cs);
+}
+
+refreshColorScheme();
+document.getElementById("color-scheme-toggle").addEventListener("click", toggleColorScheme);
